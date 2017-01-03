807319aea49ac74e9fc920a753cfa929218c87cf
FRANCHI Loic <lfranchi@cesariot.pedago.ipb.fr>
2017-01-03 14:04:00 +0100

    initialize phase 2

---
+++ Phase2/dsmexec.c
@@ -0,0 +1,276 @@
+#include "common_impl.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+
+/* variables globales */
+
+/* un tableau gerant les infos d'identification */
+/* des processus dsm */
+dsm_proc_t *proc_array = NULL;
+
+/* le nombre de processus effectivement crees */
+volatile int num_procs_creat = 0;
+
+//Variable pour stocker le statut de sortie des fils
+sig_atomic_t child_exit_status;
+
+// Renvoie un message indiquant le format d'entrée du programme
+void usage(void){
+	fprintf(stdout,"Usage : dsmexec machine_file executable arg1 arg2 ...\n");
+	fflush(stdout);
+	exit(EXIT_FAILURE);
+}
+
+void sigchld_handler(int sig)
+{
+	/* on traite les fils qui se terminent */
+	/* pour eviter les zombies */
+	int status;
+	waitpid (-1, &status, WNOHANG);
+	/* Stocke la statut de sortie du dernier dans une variable globale.  */
+	child_exit_status = status;
+
+	printf("Processus fils traité\n"); //Indique qu'on libère bien les ressources
+}
+
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3){
+		usage();
+	}
+	else {
+		pid_t pid;
+		int num_procs = 0;
+		int i;
+
+		char **newargv=NULL;
+
+		int (*tube_stderr)[2];
+		int (*tube_stdout)[2];
+		fd_set readfs;
+		fd_set masterfs;
+
+
+		int sock_serv;
+		int port_serv;
+		int sock_acc;
+		struct sockaddr_in addr_acc;
+		socklen_t addr_acc_len = sizeof(addr_acc);
+
+		char buffer[SIZE_MSG];
+		char hostname[SIZE_MSG] = {0};
+
+		char sortie_out[500],sortie_err[500];
+		//Memset \0 pour éviter les probleme d'affichage
+		memset(&sortie_out,'\0',500);
+		memset(&sortie_err,'\0',500);
+
+
+		struct sigaction sigchld_action;
+
+		char** tab_name = NULL;
+
+		/* Mise en place d'un traitant pour recuperer les fils zombies*/
+		memset(&sigchld_action, 0, sizeof (sigchld_action));
+		sigchld_action.sa_handler = &sigchld_handler;
+
+		/* Gère SIGCHLD en appelent sigchld_handler. */
+		sigaction(SIGCHLD, &sigchld_action, NULL);
+
+		/* lecture du fichier de machines */
+		/* 1- on recupere le nombre de processus a lancer */
+		/* 2- on recupere les noms des machines : */
+		tab_name = set_data_from_file("machine_file", tab_name,&num_procs);
+		/* la machine est un des elements d'identification */
+		/* creation de la socket d'ecoute */
+		port_serv = initListeningSocket(&sock_serv, num_procs, hostname);
+		printf("fd sock_serv %d\n", sock_serv);
+		/* + ecoute effective */
+
+		tube_stderr=malloc(num_procs*sizeof(int));
+		tube_stdout=malloc(num_procs*sizeof(int));
+
+		proc_array = malloc(sizeof(proc_array) * num_procs);
+		if (proc_array == NULL){
+			ERROR_EXIT("Erreur malloc");
+		}
+
+		/* creation des fils */
+		for(i = 0; i < num_procs ; i++) {
+
+			/* creation du tube pour rediriger stdout */
+			if (pipe(tube_stdout[i]) == -1) {
+				perror("Le pipe stdout n'a pas fonctionné");
+			}
+
+			/* creation du tube pour rediriger stdout */
+			if (pipe(tube_stderr[i]) == -1) {
+				perror("Le pipe stderr n'a pas fonctionné");
+			}
+
+
+			pid = fork();
+			if(pid == -1) ERROR_EXIT("fork");
+
+			if (pid == 0) { /* fils */
+
+				close(sock_serv); // le fils n'a pas besoin de la socket d'ecoute du pere serveur
+				/* redirection stdout */
+
+
+				close(tube_stdout[i][0]);          /* Fermeture du coté lecture non utilisé par le fils */
+				dup2(tube_stdout[i][1],STDOUT_FILENO);     /* On affecte stdout à l'extrémité 1 du tube (écriture)*/
+
+				/* redirection stderr */
+				close(tube_stderr[i][0]);          /* Fermeture du coté lecture non utilisé par le fils */
+				dup2(tube_stderr[i][1],STDERR_FILENO);     /* On affecte stderr à l'extrémité 1 du tube (écriture)*/
+
+				/* Creation du tableau d'arguments pour le ssh */
+
+
+
+				newargv = init_SshArg(tab_name[i],port_serv,hostname,argc,argv);
+
+				/* jump to new prog : */
+				execvp("ssh",newargv);
+			}
+			else  if(pid > 0) { /* pere */
+
+				close(tube_stdout[i][1]);          /* Fermeture du coté écriture non utilisé par le pere */
+				close(tube_stderr[i][1]);          /* Fermeture du coté écriture non utilisé par le pere */
+
+				num_procs_creat++;
+			}
+		}
+
+
+		for(i = 0; i < num_procs_creat ; i++){
+
+			/* on accepte les connexions des processus dsm */
+
+			/* ACCEPT SOCKET */
+
+			//accept connection from client
+			do{
+				sock_acc = accept(sock_serv, (struct sockaddr*) & addr_acc, &addr_acc_len);
+			}
+			while(sock_acc <0 );
+			
+
+
+			/*  On recupere le nom de la machine distante */
+			/* 1- d'abord la taille de la chaine */
+			if (read(sock_acc, buffer, SIZE_MSG) < 0){
+				ERROR_EXIT("Erreur read");
+			}
+			proc_array[i].connect_info.machine = malloc(atoi(buffer)*sizeof(char));
+
+			/* 2- puis la chaine elle-meme */
+			memset(buffer, '\0', SIZE_MSG);
+			if (read(sock_acc, buffer, SIZE_MSG) < 0){
+				ERROR_EXIT("Erreur read");
+			}
+
+			sprintf(proc_array[i].connect_info.machine, "%s", buffer);
+			printf("machine = %s\n", proc_array[i].connect_info.machine);
+
+			/* On recupere le pid du processus distant  */
+			memset(buffer, '\0', SIZE_MSG);
+			if (read(sock_acc, buffer, SIZE_MSG) < 0){
+				ERROR_EXIT("Erreur read");
+			}
+
+			proc_array[i].pid = atoi(buffer);
+			printf("pid = %d\n", proc_array[i].pid);
+
+
+
+			/* On recupere le numero de port de la socket */
+			/* d'ecoute des processus distants */
+			memset(buffer, '\0', SIZE_MSG);
+			if (read(sock_acc, buffer, SIZE_MSG) < 0){
+				ERROR_EXIT("Erreur read");
+			}
+			proc_array[i].connect_info.listenning_port = atoi(buffer);
+			printf("port = %d\n", proc_array[i].connect_info.listenning_port);
+
+
+			proc_array[i].connect_info.rank = i;
+			printf("rank = %d\n", proc_array[i].connect_info.rank);
+
+		}
+
+		/* envoi du nombre de processus aux processus dsm*/
+
+		/* envoi des rangs aux processus dsm */
+
+		/* envoi des infos de connexion aux processus */
+
+
+
+		/* gestion des E/S : on recupere les caracteres */
+		/* sur les tubes de redirection de stdout/stderr */
+
+		//clean the set before adding file descriptors
+		FD_ZERO(&readfs);
+		FD_ZERO(&masterfs);
+		// Ajout des tubes au conteneur de socket
+		for (i = 0; i < num_procs_creat; i ++){
+			FD_SET(tube_stdout[i][0], &masterfs);
+			FD_SET(tube_stderr[i][0], &masterfs);
+		}
+
+		while (1){
+			readfs=masterfs;
+			if (select(FD_SETSIZE, & readfs, NULL, NULL, NULL) <= 0) {
+				ERROR_EXIT("select");
+				break;
+			}
+
+			for (i = 0; i < num_procs_creat; i ++){
+				if (FD_ISSET(tube_stdout[i][0], & readfs)) {
+
+					if (read(tube_stdout[i][0], sortie_out, sizeof(sortie_out)) > 0){
+						printf("[Proc %d : %s : stdout]  \n%s\n",i+1,tab_name[i], sortie_out);
+						memset(&sortie_out,'\0',strlen(sortie_out));
+					}
+				}
+
+				if (FD_ISSET(tube_stderr[i][0], & readfs)) {
+
+					if (read(tube_stderr[i][0], sortie_err, sizeof(sortie_err)) > 0){
+						printf("[Proc %d : %s : stderr] \n%s\n",i+1,tab_name[i], sortie_err);
+						memset(&sortie_err,'\0',strlen(sortie_err));
+					}
+				}
+			}
+		}
+
+
+
+
+		close(tube_stdout[i][0]); // fermeture du coté lecture du tube une fois la lecture une fois que la fonction read a renvoyé 0 : EOF, la lecture terminée
+		close(tube_stderr[i][0]);
+		/*Affichage de la sortie des processus fils*/
+		printf("\n\n");
+		//Nettoie les chaines de caractères utilisées pour afficher les sortie des processus fils
+		memset(sortie_out,'\0',strlen(sortie_err));
+		memset(sortie_out,'\0',strlen(sortie_err));
+		/* on attend les processus fils */
+		wait(NULL);
+
+
+		/* on ferme les descripteurs proprement */
+
+		/* on ferme la socket d'ecoute */
+	}
+	exit(EXIT_SUCCESS);
+}
---
+++ Phase2/dsmwrap.c
@@ -0,0 +1,102 @@
+#include "common_impl.h"
+
+int main(int argc, char **argv)
+
+{
+    struct sockaddr_in sock_host;
+    char hostname[SIZE_MSG];
+    char machine[SIZE_MSG];
+    char len_machine[SIZE_MSG];
+
+    int port = atoi(argv[1]);
+    int sock;
+    char pid[SIZE_MSG];
+    char port_p2p[SIZE_MSG];
+    int sock_p2p_litsen;
+
+
+    int len_newargv=argc-3;
+    char ** newargv=malloc (len_newargv * sizeof(char*));
+
+    /* processus intermediaire pour "nettoyer" */
+    /* la liste des arguments qu'on va passer */
+    /* a la commande a executer vraiment */
+    newargv = argv+3;
+
+    /* creation d'une socket pour se connecter au */
+    /* au lanceur et envoyer/recevoir les infos */
+
+    /* necessaires pour la phase dsm_init */
+    sock = socket(AF_INET,SOCK_STREAM, IPPROTO_TCP);
+    if (sock < 0){
+        ERROR_EXIT("erreur socket");
+    }
+
+    sprintf(hostname,"%s",argv[2]);
+    sprintf(pid,"%d",getpid());
+
+    //connect to remote socket
+
+
+    do_connect(sock,hostname, port, &sock_host);
+
+
+
+    gethostname(machine, SIZE_MSG);
+    fprintf(stdout,"\n");
+    fprintf(stdout,"port:%d de %s\n", port, machine);
+    fprintf(stdout,"hostname:%s de %s\n", hostname, machine);
+    fprintf(stdout,"pid:%s de %s\n", pid, machine);
+    fprintf(stdout,"\n");
+
+    /* Envoi du nom de machine au lanceur */
+    sprintf(len_machine, "%d", (int) strlen(machine));
+    if (write(sock, len_machine, SIZE_MSG) < 0){
+        ERROR_EXIT("erreur write");
+    }
+    if (write(sock, machine, SIZE_MSG) < 0){
+        ERROR_EXIT("erreur write");
+    }
+
+    /* Envoi du pid au lanceur */
+    if (write(sock, pid,SIZE_MSG) < 0){
+        ERROR_EXIT("erreur write");
+    }
+
+    /* Creation de la socket d'ecoute pour les */
+    /* connexions avec les autres processus dsm */
+    sprintf(port_p2p, "%d", initListeningSocket(&sock_p2p_litsen, 3, machine));
+    fprintf(stdout,"portlisten:%s de %s\n", port_p2p, machine);
+
+    /* Envoi du numero de port au lanceur */
+    /* pour qu'il le propage à tous les autres */
+    /* processus dsm */
+    if (write(sock, port_p2p,SIZE_MSG) < 0){
+        ERROR_EXIT("erreur write");
+    }
+
+
+    fflush(stdout);
+    fflush(stderr);
+
+
+    /* on execute la bonne commande */
+    execvp("Documents/2A/PR204/PR204/Phase_1/bin/truc",newargv);
+
+    FILE* fichier = NULL;
+    fichier = fopen("Documents/2A/PR204/PR204/Phase_1/titi", "a");
+    if (fichier == NULL)
+    {
+        ERROR_EXIT("erreur write");
+    }
+    fputs("\n\n", fichier);
+    fputs(machine, fichier);
+    fflush(fichier);
+    fclose(fichier);
+    return 0;
+/*
+
+   execvp("Semestre_7/PR204/Phase_1/bin/truc",newargv);
+   return 0;
+*/
+}
